419559f746126fa311426202fbc4ffcaacde63c6
shg1998 <moh.nezh1377@gmail.com>
2023-07-15 15:59:19 +0430

    add Basket Service with Redis db :)

---
+++ Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart() { }
+
+        public ShoppingCart(string username) => this.Username = username;
+
+        public string Username { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in this.Items)
+                    totalPrice += item.Price * item.Count;
+                return totalPrice;
+            }
+        }
+    }
+}
