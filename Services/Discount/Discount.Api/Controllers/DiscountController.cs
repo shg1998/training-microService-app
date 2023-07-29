68091545e3e513724a8999c1581df3c841b20473
shg1998 <moh.nezh1377@gmail.com>
2023-07-29 15:26:30 +0330

    Added Discount Apis using PostgreSql :)

---
+++ Services/Discount/Discount.Api/Controllers/DiscountController.cs
@@ -0,0 +1,58 @@
+﻿using Discount.Api.Entities;
+using Discount.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Discount.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{productName}")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var cop = await this._repository.GetDiscount(productName);
+            return Ok(cop);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount(Coupon coupon)
+        {
+            var stat = await this._repository.CreateDiscount(coupon);
+            if (stat)
+                return Ok(coupon);
+            return BadRequest();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount(Coupon coupon)
+        {
+            var stat = await this._repository.UpdateDiscount(coupon);
+            if (stat)
+                return Ok(coupon);
+            return BadRequest();
+        }
+
+        [HttpDelete("{productName}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult> DeleteDiscount(string productName)
+        {
+            var stat = await this._repository.DeleteDiscount(productName);
+            if (stat)
+                return Ok();
+            return BadRequest();
+        }
+    }
+}
