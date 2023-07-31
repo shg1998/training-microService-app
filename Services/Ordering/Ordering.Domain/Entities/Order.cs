a51269ff03d84e0601b1e9b3642c037dc90ec896
shg1998 <moh.nezh1377@gmail.com>
2023-07-31 18:11:37 +0330

    added Ordering Project using CQRS Pattern And DDD :)

---
+++ Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using Ordering.Domain.Common;
+
+namespace Ordering.Domain.Entities
+{
+    public class Order : BaseEntity
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string BankName { get; set; }
+        public string RefCode { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}
