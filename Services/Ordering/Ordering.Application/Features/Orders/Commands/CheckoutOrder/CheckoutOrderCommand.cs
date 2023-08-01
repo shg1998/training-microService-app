39203d6a3009a0a009f7f1342fe29cefca38febe
shg1998 <moh.nezh1377@gmail.com>
2023-08-01 10:36:40 +0330

    remaind files added :)

--- Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommand.cs
+++ Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommand.cs
@@ -4,6 +4,16 @@ namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
 {
     public class CheckoutOrderCommand: IRequest<int>
     {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string BankName { get; set; }
+        public string RefCode { get; set; }
+        public int PaymentMethod { get; set; }
 
     }
 }
