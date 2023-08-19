4b28de8b2fc273dd6f0bfffbbbaac3e7d9425097
shg1998 <moh.nezh1377@gmail.com>
2023-08-19 19:10:09 +0330

    added subscriber implementation |:)

--- Services/Ordering/Ordering.Api/EventBusConsumer/BasketCheckoutConsumer.cs
+++ Services/Ordering/Ordering.Api/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -1,13 +1,28 @@
-﻿using EventBus.Messages.Events;
+﻿using AutoMapper;
+using EventBus.Messages.Events;
 using MassTransit;
+using MediatR;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 
 namespace Ordering.Api.EventBusConsumer
 {
     public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
     {
-        public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+        private readonly ILogger<BasketCheckoutConsumer> _logger;
+        public BasketCheckoutConsumer(IMapper mapper, IMediator mediator, ILogger<BasketCheckoutConsumer> logger)
         {
-            throw new NotImplementedException();
+            this._mapper = mapper;
+            this._mediator = mediator;
+            this._logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var cmd = this._mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await this._mediator.Send(cmd);
+            this._logger.LogInformation($"order consumed successfully and it is : {result}");
         }
     }
 }
--- Services/Ordering/Ordering.Api/Program.cs
+++ Services/Ordering/Ordering.Api/Program.cs
@@ -24,7 +24,8 @@ builder.Services.AddMassTransit(config =>
 });
 
 builder.Services.AddMassTransitHostedService();
-
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<BasketCheckoutConsumer>();
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
